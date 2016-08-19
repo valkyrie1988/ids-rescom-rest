6a2abb57708f6dcfe18fd388d40a6766eaa933df
Calvin Koh <kohcw1988@gmail.com>
2016-08-20 02:25:34 +0800

    -daily commit
    Signed-off-by: jeffreytan <jeffreytan@192.168.0.101>

--- src/main/java/com/ids/rescom/controller/AdvertisementController.java
+++ src/main/java/com/ids/rescom/controller/AdvertisementController.java
@@ -2,9 +2,12 @@ package com.ids.rescom.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,14 @@ public class AdvertisementController {
 	
 		log.info("getAdvertisements returns {}", advertisements);
 		return advertisements;
+    }
+	
+	
+	@RequestMapping(value = "/advertisements/{id}", method = RequestMethod.GET)
+    public Advertisement getAdvertisementByID(@PathVariable Long id) {
+		Advertisement advertisement = adsRepo.findOne(id);
+	
+		log.info("getAdvertisements returns {}", advertisement);
+		return advertisement;
     }
 }
